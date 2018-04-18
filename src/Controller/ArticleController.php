08cb41e0c4af7f82e8b61a32b130eece41d2a366
s.rodionov <s.rodionov@itransition.com>
2018-04-18 16:26:55 +0300

    Make like button work.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -35,6 +38,16 @@ class ArticleController extends AbstractController
 		return $this->render('article/show.html.twig', [
 			'title' => ucwords(str_replace('_', ' ', $slug)),
 			'comments' => $comments,
+			'slug' => $slug,
 		]);
+	}
+
+	/**
+	 * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+	 */
+	public function toggleArticleHeart($slug, LoggerInterface $logger)
+	{
+		$logger->info('Article being hearted!');
+		return new JsonResponse(['hearts' => rand(5,100)]);
 	}
 }
